7e0fcc20e493724f84b6d0b1cab3e1c24e4634bd
unknown <Administrador@NORTECQUIMICA.COM>
2020-05-27 12:24:48 -0300

    Add List in Home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,59 +1,121 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Container, CardGroup } from 'react-bootstrap';
-import { FaUserAlt, FaUserTimes, FaUserClock, FaUserEdit, FaUserCheck } from 'react-icons/fa';
+import { FaUserAlt, FaUserTimes, FaUserClock, FaUserEdit, FaUserCheck, FaUserCircle } from 'react-icons/fa';
 
-import { CardHome } from './styles';
+import { CardHome, TableHome } from './styles';
 
-export default function Home() {
+function CardIcon({ icon }) {
+  switch(icon) {
+    case 'FaUserAlt':
+      return <FaUserAlt size={32} color="#4CAF50" />;
+    case 'FaUserTimes':
+      return <FaUserTimes size={32} color="#4CAF50" />;
+    case 'FaUserClock':
+      return <FaUserClock size={32} color="#4CAF50" />;
+    case 'FaUserEdit':      
+      return <FaUserEdit size={32} color="#4CAF50" />;
+    case 'FaUserCheck':
+      return <FaUserCheck size={32} color="#4CAF50" />;
+    default:
+      return <FaUserCircle size={32} color="#4CAF50" />;
+  };
+}
+
+function Cards({ cards }) {
   return (
-    <Container fluid="md">
-      <CardGroup>
-        <CardHome onClick={() => {console.log('teste');}}>
+    <CardGroup>
+      { cards.map(card => (
+        <CardHome key={card.title} onClick={() => {console.log('teste');}}>
           <CardHome.Body>
-            <CardHome.Title>Avaliações abertas</CardHome.Title>            
-            <CardHome.Text>              
-              <FaUserAlt size={32} color="#4CAF50" />
-              <span>50</span>
+            <CardHome.Title>{card.title}</CardHome.Title>            
+            <CardHome.Text>
+              <CardIcon icon={card.icon}/>
+              <span>{card.total}</span>
             </CardHome.Text>
           </CardHome.Body>
         </CardHome>
-        <CardHome onClick={() => {console.log('teste');}}>
-          <CardHome.Body>
-            <CardHome.Title>Reprovadas</CardHome.Title>            
-            <CardHome.Text>              
-              <FaUserTimes size={32} color="#4CAF50" />
-              <span>50</span>
-            </CardHome.Text>
-          </CardHome.Body>
-        </CardHome> 
-        <CardHome onClick={() => {console.log('teste');}}>
-          <CardHome.Body>
-            <CardHome.Title>Em aprovação</CardHome.Title>            
-            <CardHome.Text>              
-              <FaUserClock size={32} color="#4CAF50" />
-              <span>50</span>
-            </CardHome.Text>
-          </CardHome.Body>
-        </CardHome> 
-        <CardHome onClick={() => {console.log('teste');}}>
-          <CardHome.Body>
-            <CardHome.Title>Avaliar</CardHome.Title>            
-            <CardHome.Text>              
-              <FaUserEdit size={32} color="#4CAF50" />
-              <span>50</span>
-            </CardHome.Text>
-          </CardHome.Body>
-        </CardHome>
-        <CardHome onClick={() => {console.log('teste');}}>
-          <CardHome.Body>
-            <CardHome.Title>Finalizadas</CardHome.Title>            
-            <CardHome.Text>              
-              <FaUserCheck size={32} color="#4CAF50" />
-              <span>50</span>
-            </CardHome.Text>
-          </CardHome.Body>
-        </CardHome> 
-      </CardGroup>
-    </Container>
+      )) }      
+    </CardGroup>
   );
+}
+
+function List({ colaboradores }) {
+  return (
+    <TableHome striped bordered hover>
+      <thead>
+        <tr>
+          <th></th>
+          <th>Nome</th>
+          <th>Departamento</th>
+          <th>Cargo</th>
+        </tr>
+      </thead>
+      <tbody>
+        {colaboradores.map(colaborador => (
+          <tr key={String(colaborador.id)}>
+            <td>{colaborador.id}</td>
+            <td>{colaborador.name}</td>
+            <td>{colaborador.departamento}</td>
+            <td>{colaborador.cargo}</td>
+          </tr>
+        ))}        
+      </tbody>
+    </TableHome>
+  );
+}
+
+export default class Home extends Component {
+  state = {
+    cards: [
+      {
+        title: 'Avaliações abertas',
+        icon: 'FaUserAlt',
+        total: 14,
+      },
+      {
+        title: 'Reprovados',
+        icon: 'FaUserTimes',
+        total: 1,
+      },
+      {
+        title: 'Em aprovação',
+        icon: 'FaUserClock',
+        total: 3,
+      },
+      {
+        title: 'Avaliar',
+        icon: 'FaUserEdit',
+        total: 0,
+      },
+      {
+        title: 'Finalizadas',
+        icon: 'FaUserCheck',
+        total: 2,
+      }
+    ],
+    colaboradores: [
+      {
+        id: 1,
+        name: 'João da Silva',
+        departamento: 'Informática',
+        cargo: 'Técnico'
+      },
+      {
+        id: 2,
+        name: 'Fulano Pereira',
+        departamento: 'Informática',
+        cargo: 'Analista'
+      }
+    ],
+  };
+  render()  {
+    const { cards, colaboradores } = this.state;
+
+    return (    
+      <Container fluid="md">
+        <Cards cards={cards}/>
+        <List colaboradores={colaboradores}/>
+      </Container>
+    );
+  }  
 }
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,9 +1,18 @@
 import styled from 'styled-components';
-import { Card } from 'react-bootstrap';
+import { Card, Table } from 'react-bootstrap';
 
-export const CardHome = styled(Card)`  
+export const CardHome = styled(Card)`
+  &:hover {
+    cursor: pointer;
+  }
+
   svg { 
     color: #4CAF50;    
     margin: 0 40px 0 20px;
   }
+`;
+
+export const TableHome = styled(Table)`
+  background: white;
+  margin-top: 10px;
 `;
